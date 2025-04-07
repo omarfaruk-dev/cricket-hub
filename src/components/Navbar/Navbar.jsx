e9d3abe1651aaa1019c9277b3156f792d424a5f5
Omar Faruk <omarfaruk.dev@gmail.com>
2025-04-07 12:30:43 +0600

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
     return (
         <div>
-            <nav className=' mt-6 flex justify-between items-center'>
+            <nav className=' mt-6 flex justify-between items-center px-5'>
                 <img src="images/logo.svg" alt="" />
-                <ul className='flex justify-between items-center gap-10 font-normal'>
+                <ul className='hidden md:flex justify-between items-center gap-10 font-normal'>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Fixture</a></li>
                     <li><a href="#">Teams</a></li>
                     <li><a href="#">Schedules</a></li>
-                    <button className='btn'><span>0</span> Coin <img src="images/coin.svg" alt="" /></button>
+                    <button className='btn py-4 px-5 rounded-xl bg-white'><span>0</span> Coin <img src="images/coin.svg" alt="" /></button>
                 </ul>
+                <span class="md:hidden"><HiMenuAlt3 size={25}/></span>
             </nav>
         </div>
     );
