507523149d9c33a01c98e0db382510ba385c1500
Omar Faruk <omarfaruk.dev@gmail.com>
2025-04-07 12:11:02 +0600

    initial file setup and add resources files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className=' mt-6 flex justify-between items-center'>
+                <img src="images/logo.svg" alt="" />
+                <ul className='flex justify-between items-center gap-10'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Fixture</a></li>
+                    <li><a href="#">Teams</a></li>
+                    <li><a href="#">Schedules</a></li>
+                    <button className='btn'><span>0</span> Coin <img src="images/coin.svg" alt="" /></button>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
